3bb3dfdfbc3863c26238f6e1d8a20bdd8e466e12
ltciro <ltciro@gmail.com>
2016-07-13 11:47:13 -0500

    finish CRUD comments and partial

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -9,6 +9,22 @@ class CommentsController < ApplicationController
 		comment.save
 		redirect_to post_path(@post)
 	end
+	def edit 
+		@post= Post.find(params[:post_id])
+		@comment= @post.comments.find(params[:id])
+	end
+	def update
+    @post= Post.find(params[:post_id])
+    comment= @post.comments.find(params[:id])
+		comment.update(comment_params)
+  	redirect_to post_path(@post)
+  end
+  def destroy
+  	@post= Post.find(params[:post_id])
+  	comment= @post.comments.find(params[:id])
+  	comment.destroy
+  	redirect_to post_path(@post)
+  end
 
 
 	private 
