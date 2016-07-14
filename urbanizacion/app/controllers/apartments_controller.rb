67313613ef97be45022737009f723bb17e8d6112
ltciro <ltciro@gmail.com>
2016-07-14 17:23:41 -0500

    Refactoring code and form modal

--- urbanizacion/app/controllers/apartments_controller.rb
+++ urbanizacion/app/controllers/apartments_controller.rb
@@ -1,40 +1,50 @@
 class ApartmentsController < ApplicationController
+	before_action :set_tower 
+	before_action :set_apartment , only: [:update, :destroy]
 
 	def index
-		@tower= Tower.find(params[:tower_id])
+		
   	@apartments=@tower.apartments.all
   end
 
   def new
-  	@tower= Tower.find(params[:tower_id])
+  	
   	@apartment= @tower.apartments.new
   end
 
-  def create
-  	@tower= Tower.find(params[:tower_id])
-  	apartment= @tower.apartments.new(apartment_params)
-  	apartment.save
-  	redirect_to  tower_apartments_path(@tower)
+  def create  	
+  	@apartment= @tower.apartments.new(apartment_params)  	
+  	if @apartment.save
+  		redirect_to  tower_apartments_path(@tower), notice: "Exitoso"
+  	else
+  		render "new"
+  	end
+
+
+  	
   end
   def edit 
-  	@tower= Tower.find(params[:tower_id])
+  	
   	@apartment = @tower.apartments.find(params[:id])
   end
   def update
-  	@tower= Tower.find(params[:tower_id])
-  	apartment = Apartment.find(params[:id])
   	apartment.update(apartment_params)
   	redirect_to tower_apartments_path(@tower)
 
   end
   def destroy
-  	@tower= Tower.find(params[:tower_id])
-  	apartment=Apartment.find(params[:id])
   	apartment.destroy
   	redirect_to tower_apartments_path(@tower)
 
   end
    private
+
+   def set_tower
+   	@tower= Tower.find(params[:tower_id])
+   end
+    def set_apartment
+   	apartment = Apartment.find(params[:id])
+   end
 
    def apartment_params
    	params.require(:apartment).permit(:id,:name,:floor, :tower_id)
--- urbanizacion/app/models/apartment.rb
+++ urbanizacion/app/models/apartment.rb
@@ -1,4 +1,5 @@
 class Apartment < ActiveRecord::Base
 	has_many :people
 	belongs_to :tower
+	validates  :name , presence: true
 end
